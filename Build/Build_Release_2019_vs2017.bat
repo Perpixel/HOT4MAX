3fe384dc2deb5111e29f62e53570898a822b8dc3
Guillaume Plourde <gplourde@gmail.com>
2018-12-21 16:35:47 -0500

    Build script update for new releases

---
+++ Build/Build_Release_2019_vs2017.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+pushd ..\
+
+set SdkVersion=2019
+set Toolset=v141
+set MSBUILD_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin"
+
+set MaxSdkDir=..\3rdparty\Autodesk\%SdkVersion%
+set Dependencies=comctl32.lib;mnmath.lib;paramblk2.lib;core.lib;geom.lib;gfx.lib;maxutil.lib;poly.lib;mesh.lib;bmm.lib;assetmanagement.lib;blitz.lib;libfftw3f-3.lib
+
+%MSBUILD_PATH%\MSBuild.exe .\vs2012\hotOcean_Max.vcxproj /m /p:Configuration=Release;Platform=x64
+
+pause
+
+del Hot4Max_%SdkVersion%_64bit.zip
+.\3rdparty\7z\7z a -tzip .\Release\Hot4Max_%SdkVersion%_64bit.zip readme.html
+.\3rdparty\7z\7z a -tzip .\Release\Hot4Max_%SdkVersion%_64bit.zip .\3rdparty\fftw-3.3.5\libfftw3f-3.dll
+.\3rdparty\7z\7z a -tzip .\Release\Hot4Max_%SdkVersion%_64bit.zip .\plugins\%SdkVersion%\x64\hotOcean_Max_%SdkVersion%.dlm
+
+popd
